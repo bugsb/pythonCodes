05eb311d8be7b32a073a6ada9dc11faaddef7e69
bugsb <manishianand@gmail.com>
2020-03-11 01:23:49 +0530

    shoe passed all T.C

--- hackerRank/shoe_counter.py
+++ hackerRank/shoe_counter.py
@@ -3,8 +3,14 @@ from collections import Counter
 noOfShoes = int(input())
 shoeList = Counter(input().split(" "))
 noOfCostumer = int(input())
+sum =0
+orders = [input().split() for _ in range(noOfCostumer)] # key price
 
-orders = [input().split() for _ in range(noOfCostumer)]
+for size,price in orders:
+    for sz,no in shoeList.items():
+        if size == sz and no > 0:
+            sum += int(price)
+            shoeList[sz] = int(shoeList[sz]) -1  #itta sochna tha bas
+            
 
-print(orders)
-print(shoeList)
+print(sum)
