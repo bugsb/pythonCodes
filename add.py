b11bdb6d2a4a57a4951994803d65b93fd7d3488b
bugsb <manishianand@gmail.com>
2020-07-04 16:56:52 +0530

    config for sublime

--- add.py
+++ add.py
@@ -2,9 +2,7 @@ import datetime
 import pytz
 a = 3
 b = 3
-print(f"a+b is {a+b}")
 
 
-l = [1, 4, 5]
+print("{} + {} is :{}".format(a,b,a+b))
 
-print()
--- dunder_call.py
+++ dunder_call.py
@@ -1,13 +1,14 @@
 class A:
 
-    def __init__(self,arg1):
-        print(f"Got {arg1} at the time of constructer")
+    def __init__(self, arg1):
+        print("Got {} at the time of constructer".format(arg1))
 
-    def __call__(self,arg2):
-        print(f"Got {arg2} from object and can be called with .attr")
+    def __call__(self, arg2):
+        print("Got {} from object and can be called with .attr".format(arg2))
 
-    def fun(self,arg3):
-        print(f"Got {arg3} by calling fun method ")
+    def fun(self, arg3):
+        print("Got {} by calling fun method ".format(arg3
+        	))
 
 
 a = A('init')                # for __init__
